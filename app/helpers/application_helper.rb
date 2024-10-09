fc63a62dbe36192f382cdef272595332f90e0f26
shiva reddy <shiva.r547@gmail.com>
2024-10-09 17:24:54 +0000

    comit files for home page and popups fix

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,7 @@
 module ApplicationHelper
+  def discount_percentage(price, compare_at_price)
+    return 0 if compare_at_price.nil? || compare_at_price <= 0 || price >= compare_at_price
+
+    ((compare_at_price - price) / compare_at_price) * 100
+  end
 end
