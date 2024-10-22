0345e55dec7bd9b6ba7ab4b8a5952bbc414cedf4
shiva reddy <shiva.r547@gmail.com>
2024-10-22 13:18:01 +0000

    comit files for admin access roles

--- app/assets/config/manifest.js
+++ app/assets/config/manifest.js
@@ -8,6 +8,9 @@
 //= link spree/frontend/views/spree/shared/product_added_modal.js
 //= link controllers/index.js
 //= link controllers/application.js
+//= link_tree ../builds
+//= link_tree controllers
+
 
 
 //= link select2_locale_en.js
