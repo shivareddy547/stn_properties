dcbee01432fd7a2819c34fd74576df6d6a15d147
shiva reddy <shiva.r547@gmail.com>
2024-10-22 14:10:08 +0000

    comit files for admin access roles

--- app/assets/config/manifest.js
+++ app/assets/config/manifest.js
@@ -5,12 +5,13 @@
 //= link spree/backend/all.js
 //= link spree/frontend/all.js
 //= link spree/frontend/all.css
-//= link spree/frontend/views/spree/shared/product_added_modal.js
-//= link controllers/index.js
-//= link controllers/application.js
-//= link controllers/hello_controller.js
-
-
 //= link select2_locale_en.js
 //= link flatpickr/l10n/default.js
 // = link_tree ../builds
+//= link controllers/index.js
+//= link controllers/application.js
+//= link controllers/hello_controller.js
+//= link spree/frontend/views/spree/shared/product_added_modal.js
+
+
+
